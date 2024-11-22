398468d0095a8d7047f826a7a41c76e127a247a7
Usman-Faooq <sheikh786.ug@gmail.com>
2024-11-22 16:58:41 +0500

    Changes Done

--- app/src/main/java/com/usaclean/sportreelzz/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/usaclean/sportreelzz/ui/auth/login/LoginActivity.kt
@@ -1,10 +1,19 @@
 package com.usaclean.sportreelzz.ui.auth.login
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.Toast
 import com.usaclean.sportreelzz.R
 import com.usaclean.sportreelzz.databinding.ActivityLoginBinding
+import com.usaclean.sportreelzz.databinding.AlertForgetPasswordLayoutBinding
 import com.usaclean.sportreelzz.ui.DashboardActivity
 import com.usaclean.sportreelzz.ui.auth.signup.SignUpActivity
 
@@ -26,6 +35,45 @@ class LoginActivity : AppCompatActivity() {
             registerBtn.setOnClickListener {
                 startActivity(Intent(this@LoginActivity,SignUpActivity::class.java))
             }
+
+            forgetTV.setOnClickListener {
+                showForgetDialog()
+            }
         }
+    }
+
+    private fun showForgetDialog() {
+        val dialog = Dialog(this)
+        dialog.setCancelable(true)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        val dialogBinding = AlertForgetPasswordLayoutBinding.inflate(LayoutInflater.from(this))
+        dialog.setContentView(dialogBinding.root)
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window?.setGravity(Gravity.CENTER)
+
+        dialogBinding.submitTV.setOnClickListener {
+            val email = dialogBinding.emailET.text.toString()
+
+            if (email.isNotEmpty()){
+                //mDialog.show()
+                /*mAuth.sendPasswordResetEmail(email)
+                    .addOnSuccessListener {
+                        mDialog.dismiss()
+                        Toast.makeText(this, "Please check you email", Toast.LENGTH_SHORT).show()
+                        dialog.dismiss()
+                    }.addOnFailureListener {
+                        mDialog.dismiss()
+                        Toast.makeText(this, "Error: ${it.message}", Toast.LENGTH_SHORT).show()
+                    }*/
+                Toast.makeText(this, "Please check you email", Toast.LENGTH_SHORT).show()
+                dialog.dismiss()
+            }else{
+                Toast.makeText(this, "Email Required", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        dialog.show()
+
     }
 }
